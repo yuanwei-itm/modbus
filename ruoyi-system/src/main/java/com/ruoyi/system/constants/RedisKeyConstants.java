8426608590aa8e297446168a0e7f2f8eb8bcfe3e
wjw <422258872@qq.com>
2025-12-09 11:01:05 +0800

    redis缓存键名

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/constants/RedisKeyConstants.java
@@ -0,0 +1,9 @@
+package com.ruoyi.system.constants;
+
+/**
+ * Redis键名常量
+ */
+public class RedisKeyConstants {
+    /** 实时数据缓存键（最新一条温湿度数据） */
+    public static final String MODBUS_REALTIME_LATEST = "modbus:realtime:latest";
+}
